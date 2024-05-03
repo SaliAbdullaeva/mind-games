6293d5e40af923f8f31ade6528eeeb695211ddb1
SaliAbdullaeva <imansheyh@mail.ru>
2024-05-03 16:01:07 +0300

    Step5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,36 @@
 import java.util.Date
-
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.51.0"
     id("application")
+    id("checkstyle")
+}
+
+checkstyle {
+    toolVersion = "10.16.0" // Укажите нужную версию Checkstyle
+}
+//Пример 3. Настройка HTML-отчета
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required = false
+        html.required = true
+        html.stylesheet = resources.text.fromFile("config/checkstyle/checkstyle.xml")
+    }
+}
+//Пример 4. Включить отчет SARIF
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        sarif.required = true
+    }
+}
+//Пример 5. Настройка объема памяти, отдаваемой Checkstyle
+tasks.withType<Checkstyle>().configureEach {
+    minHeapSize = "200m"
+    maxHeapSize = "1g"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-
 //tasks.jar {
     //manifest {
         //attributes["Main-Class"] = "hexlet.code.App"
@@ -16,8 +38,6 @@ version = "1.0-SNAPSHOT"
         //attributes["Built-By"] = System.getProperty("user.name")
         //attributes["Built-Date"] = Date()
     //}
-//}
-
 repositories {
     mavenCentral()
 }
@@ -25,6 +45,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.puppycrawl.tools:checkstyle:10.16.0")
 }
 
 tasks.test {
@@ -34,3 +55,7 @@ application {
     // Указываем точку входа в приложение
     mainClass = "hexlet.code.App"
 }
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import net.sf.saxon.lib.SaxonOutputKeys;
+import net.sf.saxon.style.XSLOutput;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n " +
+                "1 - Greet \n " +
+                "0 - Exit");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            System.out.println("Your choice:" + choice);
+            Cli.greet();
+        }
     }
 }
