cf2d5e18f3ac5cd9c6ebb147e9c30a1c3abd4cfb
SaliAbdullaeva <imansheyh@mail.ru>
2024-05-30 22:28:02 +0300

    Step15

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.EvenOddGame;
+import hexlet.code.games.Calculator;
+import hexlet.code.games.Nod;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 import java.util.Scanner;
 
 public class App {
